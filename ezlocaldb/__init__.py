28931abb4740889b76e41fdea2daf402ef3eeffa
scollins <scollins@formfactor.com>
2023-09-10 16:06:23 -0700

    Add _engineDict to track engines and dispose before removing database. Added **kwark passthrough argument to get_engine. Added metadata argument to get_engine, used for db init. Added _engineDict check, before database removal. Typing fixes.

--- ezlocaldb/__init__.py
+++ ezlocaldb/__init__.py
@@ -1,19 +1,34 @@
 from pathlib import Path
+from typing import Optional, Tuple
+from collections import defaultdict
 
 import appdirs
-from sqlalchemy import Engine, create_engine as _create_engine
+from sqlalchemy import Engine, create_engine as _create_engine, MetaData
 
 DEFAULT_NAME = "Database.db"
 
+_engineDict = defaultdict(list)
 
-def get_engine(app_name: str, db_name: str = DEFAULT_NAME) -> Engine:
+
+def get_engine(
+    app_name: str,
+    db_name: str = DEFAULT_NAME,
+    metadata: Optional[MetaData] = None,
+    **kwargs,
+) -> Engine:
     """Return an SQlAlchemy Engine for a specific app"""
-    db_path = _setupDB(app_name, db_name)
-    engine = _create_engine(f"sqlite:///{str(db_path)}")
+    db_path, newDB = _setupDB(app_name, db_name)
+    engine = _create_engine(f"sqlite:///{str(db_path)}", **kwargs)
+
+    _engineDict[app_name].append(engine)
+
+    if metadata is not None and newDB:
+        metadata.create_all(engine)
+
     return engine
 
 
-def _setupDB(app_name: str, db_name: str) -> Path:
+def _setupDB(app_name: str, db_name: str) -> Tuple[Path, bool]:
     """Setup the database file in the appropriate location.
     If the file already exists, nothing is changed.
     Returns a Path object to the database"""
@@ -21,14 +36,19 @@ def _setupDB(app_name: str, db_name: str) -> Path:
     db_dir = Path(appdirs.user_data_dir(appname=app_name))
     db_dir.mkdir(parents=True, exist_ok=True)
     db_path = db_dir / db_name
+    freshDB = ~db_path.exists()
     db_path.touch(exist_ok=True)
-    return db_path
+    return db_path, freshDB
 
 
-def removeDatabase(app_name: str, db_name: str = DEFAULT_NAME):
+def removeDatabase(app_name: str, db_name: str = DEFAULT_NAME) -> None:
     """Remove database file.
 
     Also removes the data folder tree, if it is empty."""
+
+    for engine in _engineDict[app_name]:
+        engine.dispose(close=True)
+
     db_path = _setupDB(app_name, db_name)
     db_path.unlink(missing_ok=True)
     temp = db_path.parent
